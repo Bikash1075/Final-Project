e995ce635d0209b2087b1eef22e8cd18eedf5457
Biki <102693150+Bikash1075@users.noreply.github.com>
2022-11-22 20:10:37 +0530

    Add files via upload

--- Restaurant.py
+++ Restaurant.py
@@ -30,31 +30,31 @@ class Restaurant:
     def edit_food_item(self):
         try:
             
-            x = int(input("Enter the Food ID you want to update : \n"))
+            f_id = int(input("Enter the Food ID you want to update : \n"))
             
-            if x in self.food.keys():
+            if f_id in self.food.keys():
                 print("\n1) Update Food Name")
                 print("2) Update Quanity")
                 print("3) Update Price")
                 print("4) Update Discount")
                 print("5) Update Stock\n")
                 
-                y = input("Enter Your Choice(Enter the number only) : ")
+                f_details = input("Enter Your Choice(Enter the number only) : ")
                 
-                if y == "1":
-                    self.food[x]["Name"] = input("Updated Food name : ")
+                if f_details == "1":
+                    self.food[f_id]["Name"] = input("Updated Food name : ")
                     print("\n<<<<<< Successfully Updated Food Name >>>>>>\n")
-                elif y == "2":
-                    self.food[x]["Quantity"] = float(input("Updated Quantity is : "))
+                elif f_details == "2":
+                    self.food[f_id]["Quantity"] = float(input("Updated Quantity is : "))
                     print("\n<<<<<< Successfully Updated The Quantity >>>>>>\n")
-                elif y == "3":
-                    self.food[x]["Price"] = float(input("Updated Price is : "))
+                elif f_details == "3":
+                    self.food[f_id]["Price"] = float(input("Updated Price is : "))
                     print("\n<<<<<< Successfully Updated The Price >>>>>>\n")
-                elif y == "4":
-                    self.food[x]["Discount"] = float(input("Updated Discount is : "))
+                elif f_details == "4":
+                    self.food[f_id]["Discount"] = float(input("Updated Discount is : "))
                     print("\n<<<<<< successfully Updated Discount Amount >>>>>>\n")
-                elif y == "5":
-                    self.food[x]["Stock"] = float(input("Updated Stock is : "))
+                elif f_details == "5":
+                    self.food[f_id]["Stock"] = float(input("Updated Stock is : "))
                     print("\n<<<<<< Successfully Updated The Stock >>>>>>\n")
                 else:
                     print("\n!! Sorry Invalid Choice !!\n")
@@ -98,21 +98,28 @@ class Restaurant:
                 
                 print("<<< Please eneter Email Id like this 'abc@gmail.com' >>>")
                 email = input("Enter your email id : ")
-                print("<<< Password should contain atleast 1 capital,1 small,1 special char, & atleast 8-16 char long >>>")
+                print("<<< Password should contain atleast 1 capital,1 small,1 special char, & atleast 8 char long >>>")
                 password = input("Enter your password : ")
                 
-                res1 = re.findall("^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",password)
-                if res1:
+                Valid_Password = re.findall("^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&!]).*$",password)
+                if Valid_Password:
                     print("Strong password")
                 else:
                     print("Password is too weak")
                     break
                 user_name = input("Enter your full name : ")
                 phone_no = int(input("Enter your 10 digit phone number : "))
-
+                valid_Phone_num=re.match("^[6-9][0-9]{9}",phone_no)
+                if valid_Phone_num:
+                    print("Valid Phone Number")
+                else:
+                    print("Invalid Phone Number\n")
+                    print("Enter Correct Phone Number")
+                
                 address = input("Enter your address : ")
                 self.user_details = {"User Name": user_name, "Phone No.": phone_no, "Email ID": email,
                                      "Password": password, "Address": address}
+                
                 print("\n<<<<<< Your Account is Created Successfully >>>>>>\n")
                 print(f"Welcome TO {self.rest_name} Restaurant\n")
                 print("User Details : ")
@@ -127,10 +134,10 @@ class Restaurant:
         try:
             while True:
                 print(f"<_<_< Welcome TO {self.rest_name} Restaurant >_>_>\n")
-                email = input("Enter Your Email ID : ")
-                pas = input("Enter Your Password : ")
+                User_email = input("Enter Your Registered Email ID : ")
+                User_password = input("Enter Your  Password : ")
                 if len(self.user_details) != 0:
-                    if email == self.user_details["Email ID"] and pas == self.user_details["Password"]:
+                    if User_email == self.user_details["Email ID"] and User_password == self.user_details["Password"]:
                         print("\n<<<<<< Login successfully >>>>>>")
                         
                         
@@ -141,14 +148,18 @@ class Restaurant:
                             print("2) Check Order History")
                             print("3) Update Your Profile Details")
                             print("4) Exit\n")
-                            z = input("Please Input your Choice : ")
-                            if z == "1":
+                            print("Press 1 To Place New order\n")
+                            print("Press 2 To See Previously Ordered Items\n")
+                            print("Press 3 for Profile Updation\n")
+                            print("Press 4 To Take Exit from App\n")
+                            User_choice = input("Please Input your Choice : ")
+                            if User_choice == "1":
                                 self.place_order()
-                            elif z == "2":
+                            elif User_choice == "2":
                                 self.ordered_history()
-                            elif z == "3":
+                            elif User_choice == "3":
                                 self.update_details()
-                            elif z == "4":
+                            elif User_choice == "4":
                                 break
                             else:
                                 print("invalid Number")
@@ -164,28 +175,28 @@ class Restaurant:
     def place_order(self):
         try:
             if len(self.food) != 0:
-                menu = []
+                Ord_menu = []
                 for items in self.food:
-                    menu.append([self.food[items]["Name"], self.food[items]["Quantity"], self.food[items]["Price"]])
-                for i in menu:
+                    Ord_menu.append([self.food[items]["Name"], self.food[items]["Quantity"], self.food[items]["Price"]])
+                for i in Ord_menu:
                     print(i)
                 while True:
                     print("\nEnter 1 to Place the Order")
                     print("\nEnter 2 to Exit\n")
-                    x = input()
-                    if x == "1":
-                        print("Enter the Food number You want to ordered separated by comma")
-                        y = input().split(",")
-                        for i in y:
-                            z = int(i)
-                            if z <= len(menu):
-                                self.ordered_item.append(menu[z - 1])
+                    U_Opt = input()
+                    if U_Opt == "1":
+                        print("Enter the Food number You want to order, separated by comma")
+                        C_opt_list = input().split(",")
+                        for i in C_opt_list:
+                            C_Opt = int(i)
+                            if C_Opt <= len(Ord_menu):
+                                self.ordered_item.append(Ord_menu[C_Opt - 1])
                             else:
-                                print("We Don't have this Food Item : ", z)
+                                print("We Don't have this Food Item : ", C_Opt)
                         print("List of food item you selected : \n")
                         for j in self.ordered_item:
                             print(j)
-                    elif x == "2":
+                    elif U_Opt == "2":
                         break
                     else:
                         print("!! Invalid Number !!\n")
@@ -211,31 +222,31 @@ class Restaurant:
                 print("5) Address")
                 print("6) Exit\n")
 
-                x = input("Please Input your Choice : ")
-                if x == "1":
+                Update_Code = input("Please Input your Choice : ")
+                if Update_Code == "1":
                     self.user_details["User Name"] = input("Enter your updated full name : ")
                     print("\n<<<<<< Name Updated Successfully >>>>>>\n")
-                elif x == "2":
+                elif Update_Code == "2":
                     self.user_details["Phone No."] = int(input("Enter your updated 10 digit phone number : "))
                     print("\n<<<<<< Phone No. Updated Successfully >>>>>>\n")
-                elif x == "3":
+                elif Update_Code == "3":
                     self.user_details["Email_ID"] = input("Enter your updated email id : ")
                     print("\n<<<<<< Email Updated Successfully >>>>>>")
 
-                elif x == "4":
+                elif Update_Code == "4":
                     self.user_details["Password"] = input("Enter your updated password : ")
                     print("\n!<<<<<< Password Updated Successfully >>>>>>\n")
 
-                elif x == "5":
+                elif Update_Code == "5":
                     self.user_details["Address"] = input("Enter your updated address with area PIN code ")
                     print("\n<<<<<< Address Updated Successfully >>>>>>\n")
 
-                elif x == "6":
+                elif Update_Code == "6":
                     break
                 else:
                     print("\n!! Invalid Number Entered !!\n")
 
-                if x in ["1", "2", "3", '4', "5"]:
+                if Update_Code in ["1", "2", "3", '4', "5"]:
                     for i in self.user_details:
                         print(i, ":", self.user_details[i])
                 else:
@@ -258,8 +269,8 @@ def main():
             print("\n1) Admin")
             print("2) User")
             print("3) Exit\n")
-            x = input("Please Input your Choice : ")
-            if x == "1":
+            option = input("Please Input your Choice : ")
+            if option == "1":
                 # obj.check_admin()
                 read = open("admin.json","r")
                 data = json.load(read)
@@ -276,35 +287,35 @@ def main():
                         print("3) View Food Items")
                         print("4) Delete Food Items")
                         print("5) Exit\n")
-                        y = input("Please Input your Choice : ")
-                        if y == "1":
+                        Admin_option = input("Please Input your Choice : ")
+                        if Admin_option == "1":
                             obj.add_food_item()
-                        elif y == "2":
+                        elif Admin_option == "2":
                             obj.edit_food_item()
-                        elif y == "3":
+                        elif Admin_option == "3":
                             obj.view_food_item()
-                        elif y == "4":
+                        elif Admin_option == "4":
                             obj.delete_food_item()
-                        elif y == "5":
+                        elif Admin_option == "5":
                             break
                         else:
                             print("Invalid Number")
-            elif x == "2":
+            elif option == "2":
                 while True:
                     print("\nEnter the Below Options")
                     print("\n1) Register")
                     print("2) Login")
                     print("3) Exit\n")
-                    y = input("Please Input your Choice : ")
-                    if y == "1":
+                    user_option = input("Please Input your Choice : ")
+                    if user_option == "1":
                         obj.user_register()
-                    elif y == "2":
+                    elif user_option == "2":
                         obj.user_login()
-                    elif y == "3":
+                    elif user_option == "3":
                         break
                     else:
                         print("\nInvalid Number ")
-            elif x == "3":
+            elif option == "3":
                 print("\n||||/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\||||")
                 print("\n        >>>>>>>>>>>>>>>>  Thank You  <<<<<<<<<<<<<<<<<")
                 print("        >>>>>>>>>>>>  Please Visit Again  <<<<<<<<<<<<\n")
